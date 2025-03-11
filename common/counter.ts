0d155c6294e96aed58871184466593f23cd2401d
dragonish <no.web.developer@outlook.com>
2025-03-11 11:57:04 +0800

    tweak(counter): avoid potentially modify constant value

--- common/counter.ts
+++ common/counter.ts
@@ -34,7 +34,9 @@ export class Counter {
       } else {
         this.decoratorOptions = {
           ordered: false,
-          levelHeadings: decoratorOptions.levelHeadings || defaultHeadingTuple,
+          levelHeadings: decoratorOptions.levelHeadings || [
+            ...defaultHeadingTuple,
+          ],
         };
       }
     } else {
